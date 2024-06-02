dc00d89c8d34109e38862205a763ff975f030a23
AliSharjeell <ali.sharjeel2014@gmail.com>
2024-06-02 06:15:26 +0500

    create task stage

---
+++ lib/createtask.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Create extends StatelessWidget {
+  const Create({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+      child: Container(
+        padding: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 247, 247, 247),
+            borderRadius: BorderRadius.circular(25)),
+        child: Row(
+          children: [
+            Text("taskname",
+                style: GoogleFonts.poppins(
+                  fontSize: 30,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
