9411693df36a71323fd7f754b8f1c277744c7e00
AliSharjeell <ali.sharjeel2014@gmail.com>
2024-06-04 00:32:18 +0500

    Database left

--- lib/button.dart
+++ lib/button.dart
@@ -9,13 +9,14 @@ class mybutton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      minWidth: 70,
+      elevation: 0,
+      minWidth: 60,
       onPressed: onPressed,
-      color: Color.fromARGB(255, 20, 20, 20),
+      color: Color.fromARGB(255, 1, 209, 175),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
-      padding: EdgeInsets.only(top: 7, bottom: 7),
+      padding: EdgeInsets.only(top: 8, bottom: 8),
       child: Text(
         text,
         style: GoogleFonts.poppins(
--- lib/createtask.dart
+++ lib/createtask.dart
@@ -1,51 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:fulltodoapp/button.dart';
 
 class Create extends StatelessWidget {
   final controller;
   VoidCallback onSave;
-   Create({super.key,required this.controller,required this.onSave});
+  Create({super.key, required this.controller, required this.onSave});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+      padding: const EdgeInsets.only(top: 0, left: 0, right: 0),
       child: Container(
-        padding: EdgeInsets.only(top: 40, left: 40, right: 40, bottom: 20),
+        padding: EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 30),
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 247, 247, 247),
-          borderRadius: BorderRadius.circular(25),
+          color: Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(0),
         ),
-        child: Column(
-          crossAxisAlignment:
-              CrossAxisAlignment.stretch, // Stretch the children horizontally
+        child: Row(
           children: [
-            TextField(
-              controller: controller,
-              style: GoogleFonts.poppins(
-                fontSize: 20,
-              ),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                hintText: "What To Do...",
-                hintStyle: GoogleFonts.poppins(
-                  fontSize: 20,
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 12, left: 12),
+                child: TextField(
+                  controller: controller,
+                  style: GoogleFonts.poppins(
+                    fontSize: 15,
+                  ),
+                  decoration: InputDecoration(
+                    filled: true, // Enables the fill color
+                    fillColor: Color.fromARGB(255, 250, 250, 250),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Color.fromARGB(255, 0, 0, 0)),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    hintText: "What To Do...",
+                    hintStyle: GoogleFonts.poppins(
+                      fontSize: 15,
+                    ),
+                  ),
                 ),
               ),
             ),
-            SizedBox(
-                height:
-                    10), // Add vertical spacing between TextField and button
-            Align(
-              alignment: Alignment.centerRight, // Align the button to the right
-              child: mybutton(
-                text: "+",
-                onPressed: onSave,
-              ),
+            // SizedBox(width: 2),
+            mybutton(
+              text: "+",
+              onPressed: onSave,
             ),
           ],
         ),
